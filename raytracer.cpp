ddbebb98b2e6680e0fd9d2ee577129b5b491c26d
jrmccluskey <thejackmccluskey@gmail.com>
2018-12-27 12:47:27 -0500

    Added sphere to raytracer output

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "ray.h"
 
+bool hitSphere(const vec3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  double a = dot(r.direction(), r.direction());
+  double b = 2.0 * dot(oc, r.direction());
+  double c = dot(oc, oc) - radius*radius;
+  double discriminant = b*b - -4*a*c;
+  return (discriminant > 0);
+}
+
 vec3 color(const ray& r) {
+  if(hitSphere(vec3(0,0,-1), 0.5, r)) {
+    return vec3 (1,0,0);
+  }
   vec3 unitDirection = unitVector(r.direction());
   double t = 0.5*(unitDirection.y() + 1.0);
   return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
