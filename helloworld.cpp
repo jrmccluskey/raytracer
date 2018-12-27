89eabc2fb905e00e58cb46d94cb4d0c442668de8
jrmccluskey <thejackmccluskey@gmail.com>
2018-12-27 17:31:54 -0500

    Removed old hello world, replaced with class version

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include "vec3.h"
 
 int main() {
   int nx = 200;
   int ny = 100;
-  std::cout<< "P3\\n" << nx << ny << "\n255\n";
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   for(int i = ny-1; i >= 0; i--) {
-    for(int j = 0; j < nx; j++) {
-      float r = float(j) / float(nx);
-      float g = float(i) / float(ny);
-      float b = 0.2;
-      int ir = int(255.99*r);
-      int ig = int(255.99*g);
-      int ib = int(255.99*b);
+    for( int j = 0; j < nx; j++) {
+      vec3 col(double(j) / double(nx), double(i) / double(ny), 0.2);
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
+
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
