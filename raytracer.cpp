868387928e26001e0084a97da2af8c04d15fa0bd
jrmccluskey <thejackmccluskey@gmail.com>
2018-12-28 09:52:41 -0500

    Updated to include shading; introduced shading bug

--- raytracer.cpp
+++ raytracer.cpp
@@ -6,10 +6,19 @@
 #include "float.h"
 #include "camera.h"
 
+vec3 randomInUnitSphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(drand48(),drand48(), drand48()) - vec3(1,1,1);
+  } while(p.squaredLength() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
   hit_record rec;
-  if(world->hit(r, 0.0, MAXFLOAT, rec)) {
-    return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+  if(world->hit(r, 0.001, DBL_MAX, rec)) {
+    vec3 target = rec.p + rec.normal + randomInUnitSphere();
+    return 0.5 * color(ray(rec.p, target-rec.p), world);
   } else {
     vec3 unitDirection = unitVector(r.direction());
     double t = 0.5*(unitDirection.y() + 1.0);
@@ -39,6 +48,7 @@ int main() {
       }
 
       col /= double(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);
